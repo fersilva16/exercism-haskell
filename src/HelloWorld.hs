a3e1a74e12066f9f2d54e2181b611a67fdffefe6
Fernando Silva <fernando457829@gmail.com>
2022-01-18 10:55:47 -0300

    chore: start using exercism cli

--- src/HelloWorld.hs
+++
@@ -1,7 +0,0 @@
-module HelloWorld (hello) where
-
--- Write a function that returns the string "Hello, World!"
--- Source: https://exercism.org/tracks/haskell/exercises/hello-world
-
-hello :: String
-hello = "Hello, World!"
--- test/HelloWorldSpec.hs
+++
@@ -1,8 +0,0 @@
-module HelloWorldSpec where
-
-import Test.Hspec        (Spec, it, shouldBe)
-import HelloWorld (hello)
-
-spec :: Spec
-spec = it "hello" $
-          hello `shouldBe` "Hello, World!"
